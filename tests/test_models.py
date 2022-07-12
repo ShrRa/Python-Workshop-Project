f56d9f2fca28757ac9f77c5e0de893e4f3725355
ShrRa <shr.razim@gmail.com>
2022-07-12 18:01:10 +0200

    Add input check

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,13 +4,14 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
+
 @pytest.mark.parametrize(
     "test,expected",
     [
-    ([[0,0],[0,0],[0,0]],[0,0]),
-    ([[1,2],[3,4],[5,6]],[3,4]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
     ])
-def test_daily_mean(test,expected):
+def test_daily_mean(test, expected):
     """Test that mean function works for an array."""
     from inflammation.models import daily_mean
 
@@ -20,15 +21,15 @@ def test_daily_mean(test,expected):
 @pytest.mark.parametrize(
     "test,expected",
     [
-    ([[0,0],[0,0],[0,0]],[0,0]),
-    ([[1,2],[3,4],[5,6]],[5,6]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [5, 6]),
     ])
-def test_daily_max(test,expected):
+def test_daily_max(test, expected):
     """Test that max function works for an array."""
     from inflammation.models import daily_max
 
     npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
-    
+
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
@@ -36,3 +37,22 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected,expect_raises",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], None),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], None),
+        # ([[1, 1, -5], [1, 1, 1], [1, 1, 1]], [[1, 1, 0], [1, 1, 1], [1, 1, 1]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], None),
+        ([[-1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], ValueError),
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
