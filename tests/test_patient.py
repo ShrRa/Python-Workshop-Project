3fc2a85a4db6f197abcf32a67dd67cf6f1751cc5
ShrRa <shr.razim@gmail.com>
2022-07-14 17:54:59 +0200

    Add tests for Patient and Doctor classes

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,3 +8,45 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Charlie'
+    d = Doctor(name=name)
+
+    assert d.name == name
+
+def test_add_observation():
+    from inflammation.models import Patient, Observation
+
+    name = 'Charlie'
+    p = Patient(name=name)
+    obs=p.add_observation(6)
+    assert isinstance(obs,Observation)
+
+def test_add_observation_val():
+    from inflammation.models import Patient
+
+    name = 'Charlie'
+    p = Patient(name=name)
+    obs=p.add_observation(6)
+    assert obs.value == 6
+
+def test_get_patients_names():
+    from inflammation.models import Doctor,Patient,Observation
+
+    name = 'Charlie'
+    d = Doctor(name=name)
+
+    name = 'Alice'
+    p = Patient(name=name)
+    p.add_observation(5)
+    d.add_patient(p)
+    name = 'Bob'
+    p = Patient(name=name)
+    p.add_observation(12)
+    d.add_patient(p)
+
+    assert d.get_patients_names() == ['Alice', 'Bob']
